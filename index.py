717560ae08eff7cda550d2fda5d8d5d5b439ea1c
Martinus <madejo@hotmail.com>
2019-09-22 18:47:49 +0200

    v1.4.1

--- index.py
+++ index.py
@@ -344,6 +344,6 @@ def rental_car_cost(days):
     cost -= 20
   return cost
 
-def trip_cost(city, days):
-  return rental_car_cost(days) + hotel_cost(days - 1) + plane_ride_cost(city)
+def trip_cost(city, days, spending_money):
+  return rental_car_cost(days) + hotel_cost(days) + plane_ride_cost(city) + spending_money
     
