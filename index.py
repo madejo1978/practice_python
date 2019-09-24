06193bdd322e95fa6a3b93bc21ef592e432ae5ec
Martinus <madejo@hotmail.com>
2019-09-24 14:37:01 +0200

    1.5.2

--- index.py
+++ index.py
@@ -534,3 +534,12 @@ def print_list(x):
     
 print_list(n)
 
+# Multiply each element
+n = [3, 5, 7]
+
+def double_list(x):
+  for i in range(0, len(x)):
+    x[i] = x[i] * 2
+  return x
+
+print (double_list(n))
