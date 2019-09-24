f73da4950ed59481011ea1e21dd0f568e9430f7b
Martinus <madejo@hotmail.com>
2019-09-24 12:39:56 +0200

    v1.5.2

--- index.py
+++ index.py
@@ -518,3 +518,19 @@ def list_function2(x):
 
 n = [3, 5, 7]
 print (list_function2(n))
+
+# List - manipulation 
+ # add
+my_list = [1, 2, 3]
+my_list.append(4)
+print (my_list)
+
+# Print item by item
+n = [3, 5, 7]
+
+def print_list(x):
+  for i in range(0, len(x)):
+    print (x[i])
+    
+print_list(n)
+
