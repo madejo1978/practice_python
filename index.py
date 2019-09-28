78e8253e6f9ba9156b527de06d04860a3277f692
Martinus <madejo@hotmail.com>
2019-09-28 08:12:04 +0200

    v1.5.4

--- index.py
+++ index.py
@@ -649,3 +649,25 @@ while True:
   count2 += 1
   if count2 >= 10:
     break
+
+# while/else
+ # similar to if/else, difference:   
+  # the else block will execute anytime the loop condition is evaluated to False. 
+  # This means that it will execute if the loop is never entered or if the loop exits normally. 
+  # If the loop exits as the result of a break, the else will not be executed.
+
+import random
+
+print ("Lucky Numbers! 3 numbers will be generated.")
+print ("If one of them is a '5', you lose!")
+
+count3 = 0
+while count3 < 3:
+  num = random.randint(1, 6)
+  print (num)
+  if num == 5:
+    print ("Sorry, you lose!")
+    break
+  count3 += 1
+else:
+  print ("You win!")
