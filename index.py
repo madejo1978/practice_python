f255bb9b010224c2f2926932101f971a2dfaecf5
Martinus <madejo@hotmail.com>
2019-10-13 18:02:24 +0200

    v1.6.1

--- index.py
+++ index.py
@@ -818,3 +818,22 @@ panda = Animal("Chad", 7, True)
 print (zebra.name, zebra.age, zebra.is_hungry)
 print (giraffe.name, giraffe.age, giraffe.is_hungry)
 print (panda.name, panda.age, panda.is_hungry)
+
+# Class Scope
+ # global variables
+ # member variables
+ # instance variables
+class Animal3(object):
+  """Makes cute animals."""
+  is_alive = True
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+zebra = Animal3("Jeffrey", 2)
+giraffe = Animal3("Bruce", 1)
+panda = Animal3("Chad", 7)
+
+print (zebra.name, zebra.age, zebra.is_alive)
+print (giraffe.name, giraffe.age, giraffe.is_alive)
+print (panda.name, panda.age, panda.is_alive)
